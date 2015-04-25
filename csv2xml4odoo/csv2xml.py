1e57fbe0ed3dd9af543de5b9b9132498de83317e
david.beal@akretion.com <david.beal@akretion.com>
2015-04-25 19:29:30 +0200

    [IMP] add help

--- csv2xml4odoo/csv2xml.py
+++ csv2xml4odoo/csv2xml.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+# coding: utf-8
+
 # based on this code
 # http://code.activestate.com/recipes/577423-convert-csv-to-xml/
 
-# convert Odoo csv files in xml files
-# csv is easy to maintain but xml data have noupdate feature
-
-# Limitations:
-# - relation field One2many is NOT supported
-# - csv should have 'id' as first column
-# - ambiguous columns: char type but contains float string,
-#   should have special suffix on column name '|char'
-# - relationnal fields notation in csv should be:
-#   myfield_id/id for m2o or myfield_ids/id for m2m
-
-# v1.1
+# v1.2
 
 # Changelog
+# v1.2  add more args and help
 # v1.1  add args capability
 # v1    fix m2m
+
 
 import csv
 import glob
@@ -35,7 +27,33 @@ ERP_FOOTER = """
 </openerp>
 """
 
-FILES_WITH_UPDATE = ('product.product.csv')
+
+def help():
+    print """
+HELP
+----
+Use: csv2xml.py [OPTION]... [FILE]
+Convert Odoo csv files in xml files
+    csv is easy to maintain but xml data have 'noupdate' feature
+    (required for production environnement)
+
+Options:
+    -a, --all       Convert all csv files of the folder
+    -u, --update    Set 'noupdate' attribute to 0 instead of 1
+
+Limitations:
+- relation field One2many is NOT SUPPORTED
+- ambiguous columns: char type but contains float string,
+  should have special suffix on column name '|char'
+- relationnal fields notation in csv should be:
+    myfield_id/id for m2o or myfield_ids/id for m2m
+
+    """
+
+
+def check_arg(argument):
+    if '--%s' % argument in sys.argv or '-%s' % argument[0] in sys.argv:
+        return True
 
 
 def convert_relationnal_field2xml(tag, value):
@@ -54,20 +72,23 @@ def convert_relationnal_field2xml(tag, value):
 
 
 def convert_file(csv_file):
-    no_update = NOUPDATE
-    if csv_file in FILES_WITH_UPDATE:
-        no_update = 0
+    global NOUPDATE
     xml_file = csv_file.replace('.', '_').replace('_csv', '_data.xml')
     csv_data = csv.reader(open(csv_file))
     xml_data = open(xml_file, 'w')
-    xml_data.write(ERP_HEADER % no_update + "\n\n\n")
+    xml_data.write(ERP_HEADER % NOUPDATE + "\n\n\n")
     row_num = 0
-    print csv_file
     for row in csv_data:
         if row_num == 0:
             tags = row
             for i in range(len(tags)):
                 tags[i] = tags[i].replace(' ', '_')
+            if 'id' in tags:
+                id_position = tags.index('id')
+            else:
+                print ("EXCEPTION: No 'id' in %s: impossible to generate "
+                       "the xml file\n" % csv_file)
+                return
         else:
             for i in range(len(tags)):
                 char = False
@@ -83,9 +104,8 @@ def convert_file(csv_file):
                 except Exception:
                     pass
                 if tags[i] == 'id':
-                    # 'id' column is supposed to be the first left
                     line = ('<record id="%s" model="%s">\n'
-                            % (row[i], csv_file[:-4]))
+                            % (row[id_position], csv_file[:-4]))
                 elif '/' in tags[i] or ':' in tags[i]:
                     # relationnal fields
                     xml_suffix = convert_relationnal_field2xml(tags[i], row[i])
@@ -104,14 +124,34 @@ def convert_file(csv_file):
         row_num += 1
     xml_data.write(ERP_FOOTER)
     xml_data.close()
-    print "'%s' file has been created from '%s'" % (xml_file, csv_file)
-    print "        Note: 'one2many' fields are not supported by this script."
+    print "'%s' file has been created from '%s'\n" % (xml_file, csv_file)
 
 
-if len(sys.argv) > 1:
-    for csv_file in sys.argv:
-        if csv_file[-4:] in ('.csv', '.CSV'):
+def process():
+    global NOUPDATE
+    if check_arg('help'):
+        help()
+        return True
+    if len(sys.argv) == 1:
+        print 'Arguments should be provided with this script'
+        help()
+        return True
+    if check_arg('update'):
+        NOUPDATE = 0
+    if check_arg('all'):
+        for csv_file in glob.glob('*.csv'):
             convert_file(csv_file)
-else:
-    for csv_file in glob.glob('*.csv'):
-        convert_file(csv_file)
+    else:
+        number = 0
+        for csv_file in sys.argv:
+            if csv_file[-4:] in ('.csv', '.CSV'):
+                convert_file(csv_file)
+                number += 1
+        if number == 0:
+            print "No csv file provided: see Help for more information"
+            print "---------------------------------------------------"
+            help()
+
+
+if __name__ == '__main__':
+    process()
